45308472268a2ad45e5e8ab5b1936cb1a44566c8
Kalyani Dungala <itsmeekalyani06@gmail.com>
2025-10-13 20:59:16 +0530

    Add files via upload

---
+++ UNIT-3/obj as cls mem.cpp
@@ -0,0 +1,24 @@
+//  Object as a Class member
+#include <iostream>
+using namespace std;
+class Address {
+    string city;
+public:
+    Address(string c) { city = c; }
+    void show() { cout << "City: " << city << endl; }
+};
+class Student {
+    string name;
+    Address addr;   // Object as a class member
+public:
+    Student(string n, string c) : name(n), addr(c) {}
+    void display() {
+        cout << "Name: " << name << endl;
+        addr.show();
+    }
+};
+int main() {
+    Student s("Kalyani", "Tuni");
+    s.display();
+    return 0;
+}
